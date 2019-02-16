c469e6b4ada45f4f05ea30d3030508d8d4b3f3ed
Nathan <nathan@users.noreply.github.com>
2019-02-16 15:29:44 -0500

    Fix .gitignore and add some more browser examples/tests

--- pax/examples/browser/index.js
+++ pax/examples/browser/index.js
@@ -3,6 +3,8 @@ const alternateFiles = require('alternate-files/foo');
 const alternateFilesNested = require('alternate-files/nested/foo');
 const alternateFilesInternal = require('alternate-files/internal-import');
 const alternateModule = require('alternate-module');
+const alternateNesting = require('alternate-nesting');
+const alternateNesting2 = require('./node_modules/alternate-nesting/baz');
 const ignored = require('ignored');
 
 console.log('In browser should be true and false otherwise');
@@ -11,4 +13,6 @@ console.log('alternateFiles:', alternateFiles.isBrowser);
 console.log('alternateFilesNested:', alternateFilesNested.isBrowser);
 console.log('alternateFilesInternal:', alternateFilesInternal.isBrowser);
 console.log('alternateModule:', alternateModule.isBrowser);
+console.log('alternateNesting:', alternateNesting.file);
+console.log('alternateNesting2:', alternateNesting2.file);
 console.log('ignored:', ignored.isBrowser);
