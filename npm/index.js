f45ec9f183006ed148719a9e6c8fd12984caa0b2
Jake Champion <jakechampion.jake2@googlemail.com>
2019-10-28 14:56:46 +0000

    check windows package first

--- npm/index.js
+++ npm/index.js
@@ -2,13 +2,13 @@
 
 module.exports = (function(){
   try {
-    return require('oax-linux-64');
+    return require('oax-windows-64');
   } catch {
     try { 
       return require('oax-darwin');
     } catch {
       try {
-        return require('oax-windows-64');
+        return require('oax-linux-64');
       } catch {
         throw new Error('oax does not have a precompiled binary for the platform/architecture you are using. Please contact Origami or open an issue on https://github.com/Financial-Times/oax/issues');
       }
