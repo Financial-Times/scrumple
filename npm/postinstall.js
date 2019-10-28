b04fba278c78b29c34d81b633f165ebc1c2445f8
Jake Champion <jakechampion.jake2@googlemail.com>
2019-10-28 15:30:26 +0000

    Add dbeugging to postinstall

--- npm/postinstall.js
+++ npm/postinstall.js
@@ -3,6 +3,25 @@
 const fs = require('fs');
 const path = require('path');
 
+var walk = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            /* Recurse into a subdirectory */
+            results = results.concat(walk(file));
+        } else { 
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    return results;
+}
+
+console.log('test', walk(__dirname));
+
 const pathToOaxBinary = require('./index.js');
 const oaxDestination = path.join(__dirname, './oax');
 
