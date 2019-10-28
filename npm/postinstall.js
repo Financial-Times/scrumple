d5b5cd95bd8ea10c6a92b82ce3f7dad5e37508d8
Jake Champion <jakechampion.jake2@googlemail.com>
2019-10-28 15:52:57 +0000

    Do the platform and architecture detection ourselves because npm runs the postinstall script before removing all the optional dependencies which do not match the host platform and architecture

--- npm/postinstall.js
+++ npm/postinstall.js
@@ -3,26 +3,17 @@
 const fs = require('fs');
 const path = require('path');
 
-var walk = function(dir) {
-    var results = [];
-    var list = fs.readdirSync(dir);
-    list.forEach(function(file) {
-        file = dir + '/' + file;
-        var stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) { 
-            /* Recurse into a subdirectory */
-            results = results.concat(walk(file));
-        } else { 
-            /* Is a file */
-            results.push(file);
-        }
-    });
-    return results;
+let pathToOaxBinary;
+if (process.platform === "win32" && process.arch === "x64") {
+    pathToOaxBinary = require('oax-windows-64');
+} else if (process.platform === "darwin") {
+    pathToOaxBinary = require('oax-darwin');
+} else if (process.platform === "linux" && process.arch === "x64") {
+    pathToOaxBinary = require('oax-linux-64');
+} else {
+    throw new Error('oax does not have a precompiled binary for the platform/architecture you are using. Please contact Origami or open an issue on https://github.com/Financial-Times/oax/issues');
 }
 
-console.log('test', walk(__dirname));
-
-const pathToOaxBinary = require('./index.js');
 const oaxDestination = path.join(__dirname, './oax');
 
 fs.unlinkSync(oaxDestination);
