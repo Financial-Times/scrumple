65359c6306f8f649032f479a2a6c36e6bf3615d7
chee <chee@snoot.club>
2020-05-13 10:24:22 +0100

    Fix spacing on help message

--- src/main.rs
+++ src/main.rs
@@ -463,11 +463,10 @@ Options:
     -b, --for-bower
         Use bower.json instead of package.json
 
-   -N, --allow-npm-dev-deps
-   When using --for-bower, this
-        forces packages in the project's package.json#devDependencies to be
-        resolved through npm. This is is for creating testing bundles that use
-        npm-only dependencies
+    -N, --allow-npm-dev-deps
+        When using --for-bower, this forces packages in the project's
+        package.json#devDependencies to be resolved through npm. This is is for
+        creating testing bundles that use npm-only dependencies
 
     -h, --help
         Print this message.
